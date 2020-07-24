39c13bc5fe950b3f8ecc43d7fdf5e6be8812918d
ricardozv <ricardosilvanet03@gmail.com>
2020-07-23 21:37:37 -0300

    up directory src

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const ViewBoxesWithColorAndText = () => {
+  return (
+    
+    <View
+      style={{
+        flex:1,
+        alignItems:'center',
+        justifyContent:'center',
+      }}
+    >
+     <Text>drexy</Text>
+    </View>
+  );
+};
+
+export default ViewBoxesWithColorAndText;
