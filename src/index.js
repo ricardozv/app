ccc94ee8f7a8301cad9de023d73289ed4d45bcb7
ricardozv <ricardosilvanet03@gmail.com>
2020-07-25 01:46:05 -0300

    upload

--- src/index.js
+++ src/index.js
@@ -1,19 +1,13 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
 
-const ViewBoxesWithColorAndText = () => {
-  return (
-    
-    <View
-      style={{
-        flex:1,
-        alignItems:'center',
-        justifyContent:'center',
-      }}
-    >
-     <Text>drexy</Text>
-    </View>
-  );
-};
+import Navigation from './Navigation';
 
-export default ViewBoxesWithColorAndText;
+export default function App() {
+    return (
+        <NavigationContainer>
+            <Navigation />
+        </NavigationContainer>
+
+    );
+}
