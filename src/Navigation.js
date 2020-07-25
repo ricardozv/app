9794196931151b2d2754b0a5c8f2298343e89acd
ricardozv <ricardosilvanet03@gmail.com>
2020-07-25 01:45:30 -0300

    add react navigation

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { AntDesign, Ionicons } from '@expo/vector-icons';
 
 import HomeScreen from './screens/Home';
 import WalletScreen from './screens/Wallet';
@@ -7,12 +8,49 @@ import PayScreen from './screens/Pay';
 
 const Tab = createBottomTabNavigator();
 
+const icons = {
+    Home: {
+        lib: AntDesign,
+        name: 'home',
+    },
+};
+
 export default function Navigation() {
     return (
         <Tab.Navigator>
-            <Tab.Screen name="Home" component ={HomeScreen} />
-            <Tab.Screen name="Wallet" component ={WalletScreen} />
-            <Tab.Screen name="Pay" component ={PayScreen} />
+            <Tab.Screen name="Home" 
+            component ={HomeScreen} 
+            options={{
+            title: 'Início',
+            }}/>
+
+            <Tab.Screen 
+            name="Wallet" 
+            component ={WalletScreen} 
+            options={{
+            title:'Carteira' 
+            }}/>
+
+            <Tab.Screen 
+            name="Pay" 
+            component ={PayScreen} 
+            options={{
+            title:'pagar'
+            }}/>
+
+            <Tab.Screen 
+            name="Notifications" 
+            component ={PayScreen} 
+            options={{
+            title:'Notificações'
+            }}/>
+
+            <Tab.Screen 
+            name="Settings" 
+            component ={PayScreen} 
+            options={{
+            title:'Ajustes'
+            }}/>
 
         </Tab.Navigator>
     );
